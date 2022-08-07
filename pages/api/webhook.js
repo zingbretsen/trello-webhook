499f17ff51f861d381739e504821e3fffb2a107e
Zach Ingbretsen <zach@zingbretsen.com>
2022-08-07 13:26:25 -0400

    Adds webhook for adding a card to shopping list

---
+++ pages/api/webhook.js
@@ -0,0 +1,28 @@
+import fetch from "node-fetch";
+import "dotenv/config";
+
+const key = process.env.KEY;
+const token = process.env.TOKEN;
+let list_id = process.env.LISTID;
+
+export default function handler(req, res) {
+  let body = req.body;
+  let name = body.name;
+  fetch(
+    `https:api.trello.com/1/cards?idList=${list_id}&name=${name}&key=${key}&token=${token}`,
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    }
+  )
+    .then((response) => {
+      console.log(`Response: ${response.status} ${response.statusText}`);
+      return response.text();
+    })
+    .then((text) => console.log(text))
+    .catch((err) => console.error(err));
+
+  res.status(200).json({});
+}
