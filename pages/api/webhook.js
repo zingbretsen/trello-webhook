7993b7b87912ebf442e8e1befe1e854eeb58e0dc
Zach Ingbretsen <zach@zingbretsen.com>
2022-08-20 21:11:25 -0400

    try to add each item from list

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -8,21 +8,23 @@ let list_id = process.env.LISTID;
 export default async function handler(req, res) {
   let body = req.body;
   console.log(body);
-  let item = body.intent.params.item.resolved;
+  let items = body.intent.params.item.resolved;
   console.log(body.intent.params.item);
   console.log(body.scene.slots.item);
-  let result = await fetch(
-    `https:api.trello.com/1/cards?idList=${list_id}&name=${item}&key=${key}&token=${token}`,
-    {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-      },
-    }
-  )
-    .then((response) => {
-      return response.text();
-    })
-    .then((text) => res.status(200).json({}))
-    .catch((err) => res.status(500).json({}));
+  for (var i = 0; i < items.length; i++) {
+    let result = await fetch(
+      `https:api.trello.com/1/cards?idList=${list_id}&name=${item}&key=${key}&token=${token}`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    )
+      .then((response) => {
+        return response.text();
+      })
+      .then((text) => res.status(200).json({}))
+      .catch((err) => res.status(500).json({}));
+  }
 }
