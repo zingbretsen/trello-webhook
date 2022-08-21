5b4647b1a1a954fb3fa37da1c1bcbfd0bf94f899
Zach Ingbretsen <zach@zingbretsen.com>
2022-08-20 21:15:56 -0400

    modify return val

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -23,9 +23,9 @@ export default async function handler(req, res) {
       }
     )
       .then((response) => {
-        return response.text();
+        console.log(response.text());
       })
-      .then((text) => res.status(200).json({}))
       .catch((err) => res.status(500).json({}));
   }
+  return res.status(200).json({});
 }
