b026577d6e5803d1b7ffd14b136df12641fc8c7d
Zach Ingbretsen <zach@zingbretsen.com>
2022-08-24 16:09:54 -0400

    Do my own pattern matching
    rather than using google's resolved items

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -8,13 +8,22 @@ let list_id = process.env.LISTID;
 export default async function handler(req, res) {
   let body = req.body;
   console.log(body);
-  let items = body.intent.params.item.resolved;
-  console.log(body.intent.params.item);
-  console.log(body.scene.slots.item);
+
+  let query = body.intent.query;
+  let item_string = query.match(/^add (.*)/);
+  if (item_string === null) {
+    return res.status(400).json({});
+  }
+
+  let items = item_string[1]
+    .split(/,?\band\b/i)
+    .map((item) => item.replace(/,?[ ]*$/, ""));
+
   for (var i = 0; i < items.length; i++) {
     let item = items[i];
+    console.log(item);
     let result = await fetch(
-      `https:api.trello.com/1/cards?idList=${list_id}&name=${item}&key=${key}&token=${token}`,
+      `https:api.trello.com/1/cards?idList=${list_id}&description=${query}&name=${item}&key=${key}&token=${token}`,
       {
         method: "POST",
         headers: {
